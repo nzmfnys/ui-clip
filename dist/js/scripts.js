e57033cd16ae01d578301f927e55b5d775a44d2a
Popo1173 <mr.popo1173@gmail.com>
2021-09-12 20:56:10 +0900

    feat:  タブ構造変更

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -1,1 +1,21 @@
-console.log("rrrr");
+
+  $(function() {
+	  //クリックしたらイベント発火
+	$('.tab-switch__item').click(function(){
+		// デフォルト.is-activeクラスを削除
+		$('.is-active').removeClass('is-active');
+
+		//クリックしたタブにis-activeのクラス付与
+		$(this).addClass('is-active');
+
+		// デフォルト.is-show'クラスを削除
+		$('.is-show').removeClass('is-show');
+		
+		//クリックしたタブがタブの中で何番目かを判定し、indexとして定義
+		const index = $(this).index();
+		
+		//.tab-area__contents-mainが付いている要素のindex番目にis-showクラスを付与
+		$('.tab-contents__item').eq(index).addClass('is-show');
+
+	});
+});
