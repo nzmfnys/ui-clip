3f879dfd3fb519bee4cb19fb41b9d3b7691a0f24
Popo1173 <50015045+Popo1173@users.noreply.github.com>
2022-07-03 16:20:46 +0900

    bookのカルーセル検証

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -60,20 +60,39 @@ $(function(){
   PC/SP共通のため、↑のカルーセルと処理を分けた
 */
 $('.slick-add').slick({
-  autoplay: false,
   dots: true,
-  centerMode: true,
-  slidesToShow: 1,
-  slidesToScroll: 3,
-  infinite: true,
+  infinite: false,
+  speed: 300,
+  slidesToShow: 8,
+  slidesToScroll: 8,
   responsive: [
     {
-      breakpoint: 768,
+      breakpoint: 1024,
       settings: {
-        slidesToShow: 1, //画面幅750px以下でスライド3枚表示
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
       }
     }
-  ]  
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
 })
 
 $('.slick-modal').slick({
