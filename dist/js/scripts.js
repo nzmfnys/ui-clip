c7a733d412a0c39760c303f5cdd36cd4c607e4b7
Popo1173 <50015045+Popo1173@users.noreply.github.com>
2022-07-19 08:56:07 +0900

    とりあえず

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -36,11 +36,21 @@
 	});
 });
 
-/*
-  APPタブのカルーセル
-  SPの時だけイベントを発火させる
-*/
-$(function(){
+
+
+//リサイズした時に実行
+$(window).on('resize orientationchange', function() {
+  $('.slider').slick('resize');
+  
+  if ($('.hamburger').css('display') == 'block') {
+    console.log('OK')
+  } else {
+  console.log('NG')
+  }
+});
+
+window.addEventListener("load", function() {
+  /* APPタブのカルーセル SPの時だけイベントを発火させる */
   $('.slider').slick({
     infinite: true,
     mobileFirst: true,
@@ -54,58 +64,88 @@ $(function(){
       settings: 'unslick'
     } ]
   });
+  /* 広告用のカルーセル PC/SP共通のため、↑のカルーセルと処理を分けた */
+  $('.slick-add').slick({
+    autoplay: false,
+    autoplaySpeed: 0,
+    // speed: 8000,
+    cssEase: 'linear',
+    arrows:false,
+    swipe: false,
+    pauseOnFocus: false,
+    pauseOnHover: false,
+    centerMode: true,
+    initialSlide: 0,
+    variableWidth: true, 
+    dots: true,
+    // centerMode: true,
+    infinite: true,
+    slidesToShow: 1,
+    centeredSlides: true,
+    loop: true,
+    loopAdditionalSlides: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]    
+  });
 });
 
-//リサイズした時に実行
-$(window).on('resize orientationchange', function() {
-  $('.slider').slick('resize');
-  
-  if ($('.hamburger').css('display') == 'block') {
-    console.log('OK')
-  } else {
-  console.log('NG')
-  }
-});
 
-/* 
-  広告用のカルーセル
-  PC/SP共通のため、↑のカルーセルと処理を分けた
-*/
-$('.slick-add').slick({
-  dots: true,
-  infinite: true,
-  speed: 300,
-  slidesToShow: 1,
-  slidesToScroll: 1,
-  responsive: [
-    {
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 3,
-        slidesToScroll: 3,
-        infinite: true,
-        dots: true
-      }
-    },
-    {
-      breakpoint: 600,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 2
-      }
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1
-      }
-    }
-    // You can unslick at a given breakpoint now by adding:
-    // settings: "unslick"
-    // instead of a settings object
-  ]
-})
+
+// $('.slick-add').slick({
+//   dots: true,
+//   infinite: true,
+//   speed: 300,
+//   slidesToShow: 1,
+//   slidesToScroll: 1,
+//   responsive: [
+//     {
+//       breakpoint: 1024,
+//       settings: {
+//         slidesToShow: 3,
+//         slidesToScroll: 3,
+//         infinite: true,
+//         dots: true
+//       }
+//     },
+//     {
+//       breakpoint: 600,
+//       settings: {
+//         slidesToShow: 2,
+//         slidesToScroll: 2
+//       }
+//     },
+//     {
+//       breakpoint: 480,
+//       settings: {
+//         slidesToShow: 1,
+//         slidesToScroll: 1
+//       }
+//     }
+//   ]
+// })
 
 $('.slick-modal').slick({
   dots: false,
