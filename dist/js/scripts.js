fd0948f2de58df9dfe0554fc8ac4870587ef5144
Popo1173 <50015045+Popo1173@users.noreply.github.com>
2022-08-29 23:38:14 +0900

    もろもろ対応

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -2,97 +2,70 @@
 /*
   APP・WEBタブの切り替え
 */
-  $(function() {
-	  //クリックしたらイベント発火
-	// $('.tab-switch__item').click(function(){
-	// 	// デフォルト.is-activeクラスを削除
-	// 	$('.is-active').removeClass('is-active');
+//   $(function() {
+// 	// クリックしたらイベント発火　webタブできた時に使う
+// 	$('.tab-switch__item').click(function(){
+// 		// デフォルト.is-activeクラスを削除
+// 		$('.is-active').removeClass('is-active');
 
-	// 	//クリックしたタブにis-activeのクラス付与
-	// 	$(this).addClass('is-active');
+// 		//クリックしたタブにis-activeのクラス付与
+// 		$(this).addClass('is-active');
 
-	// 	// デフォルト.is-show'クラスを削除
-	// 	$('.is-show').removeClass('is-show');
+// 		// デフォルト.is-show'クラスを削除
+// 		$('.is-show').removeClass('is-show');
 		
-	// 	//クリックしたタブがタブの中で何番目かを判定し、indexとして定義
-	// 	const index = $(this).index();
+// 		//クリックしたタブがタブの中で何番目かを判定し、indexとして定義
+// 		const index = $(this).index();
 		
-	// 	//.tab-area__contents-mainが付いている要素のindex番目にis-showクラスを付与
-	// 	$('.tab-contents__item-area').eq(index).addClass('is-show');
-	// });
+// 		//.tab-area__contents-mainが付いている要素のindex番目にis-showクラスを付与
+// 		$('.tab-contents__item-area').eq(index).addClass('is-show');
+// 	});  
+// });
+
 
 /*
   バーガーメニューの開閉
 */
-	$(function() {
-		$('.hamburger').click(function() {
-			$(this).toggleClass('active');
-			if ($(this).hasClass('active')) {
-				$('.header-inner__menu').addClass('active');
-			} else {
-				$('.header-inner__menu').removeClass('active');
-			}
-		});
-	});
+$(function() {
+  $('.hamburger').click(function() {
+    $(this).toggleClass('active');
+    if ($(this).hasClass('active')) {
+      $('.header-inner__menu').addClass('active');
+    } else {
+      $('.header-inner__menu').removeClass('active');
+    }
+  });
 });
+
 
 /* APPタブのカルーセル SPの時だけイベントを発火させる */
-function checkBreakPoint() {
-	w = $(window).width();
-	if (w <= 767) {
-		// スマホ向け（767px以下のとき）
-		$('.slider-thumbnail').not('.slick-initialized').slick({
-			//スライドさせる
-			slidesToShow: 1,
-			slidesToScroll: 1,
-			arrows: false,
-			autoplay: false,
-			autoplaySpeed: 6000,
-      infinite: false,
-      centerMode: false,
-		});
-	} else {
-		// PC向け
-		$('.slider.slick-initialized').slick('unslick');
-	}
-}
-// ウインドウがリサイズする度にチェック
-$(window).resize(function(){
-	checkBreakPoint();
-});
-// 初回チェック
-checkBreakPoint();
-
-
-
-//リサイズした時に実行
-// $(window).on('resize orientationchange', function() {
-//   $('.slider').slick('resize');
-  
-//   if ($('.hamburger').css('display') == 'block') {
-//     console.log('OK')
-//   } else {
-//   console.log('NG')
-//   }
-// });
+$(function(){
+  function checkBreakPoint() {
+    w = $(window).width();
+    if (w <= 767) {
+      // スマホ向け（767px以下のとき）
+      $('.slider-thumbnail').not('.slick-initialized').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        autoplay: false,
+        autoplaySpeed: 6000,
+        infinite: false,
+        centerMode: false,
+      });
+    } else {
+      // PC向け
+      $('.slider-thumbnail.slick-initialized').slick('unslick');
+    }
+  }
+  // ウインドウがリサイズする度にチェックすして発火させる
+  $(window).resize(function(){
+    checkBreakPoint();
+  });
+  checkBreakPoint();
+})
 
 window.addEventListener("load", function() {
-  /* APPタブのカルーセル SPの時だけイベントを発火させる */
-  // $('.slider').slick({
-  //   infinite: true,
-  //   mobileFirst: true,
-  //   slidesToShow: 1,
-  //   centerMode: false,
-  //   slidesToScroll: 1,
-  //   initialSlide: 0,
-
-  //   //768px以下の時はslickを実行しない
-  //   responsive: [ {
-  //     breakpoint: 768,
-
-  //     settings: 'unslick',
-  //   } ]
-  // });
   
   /* 広告用のカルーセル PC/SP共通のため、↑のカルーセルと処理を分けた */
   $('.slick-add').slick({
@@ -141,60 +114,24 @@ window.addEventListener("load", function() {
   });
 });
 
+/*
+  モーダル内のslick 
+  デフォルトはdisplay: none のため、slickの発火を「is-modal-open」の後にする
+  モーダル表示時間に合わせて発火するように小細工
+*/ 
+$(function(){
+  var slider = $('.slick-modal').slick();
+  $('.is-modal-open').click(function(){
+      slider.css('opacity',0);
+      slider.animate({'z-index':1},100,function(){
+          slider.slick('setPosition');
+          slider.animate({'opacity':1});
+      });
+  });
+});
 
 
-
-// $('.slick-add').slick({
-//   dots: true,
-//   infinite: true,
-//   speed: 300,
-//   slidesToShow: 1,
-//   slidesToScroll: 1,
-//   responsive: [
-//     {
-//       breakpoint: 1024,
-//       settings: {
-//         slidesToShow: 3,
-//         slidesToScroll: 3,
-//         infinite: true,
-//         dots: true
-//       }
-//     },
-//     {
-//       breakpoint: 600,
-//       settings: {
-//         slidesToShow: 2,
-//         slidesToScroll: 2
-//       }
-//     },
-//     {
-//       breakpoint: 480,
-//       settings: {
-//         slidesToShow: 1,
-//         slidesToScroll: 1
-//       }
-//     }
-//   ]
-// })
-
-$('.slick-modal').slick({
-  // lazyLoad: ondemand,
-    // infinite: true,
-    // mobileFirst: true,
-    // slidesToShow: 1,
-    // centerMode: true,
-    // slidesToShow: 1,
-    // slidesToScroll: 1,
-    // centerPadding: "50px",
-    // initialSlide: 0,  
-  // dots: false,
-  // infinite: true,
-  // speed: 300,
-  // slidesToShow: 1,
-  // adaptiveHeight: true
-})
-
-// モーダル
+// モーダルウィンドウの表示、非表示
 $(function(){
   $('.is-modal-open').on('click',function(){
       $('.modal').fadeIn();
